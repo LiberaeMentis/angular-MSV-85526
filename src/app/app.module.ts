7bde692aa3412d433ba0de324a13d1b08d4c37c6
LiberaeMentis <65082399+LiberaeMentis@users.noreply.github.com>
2022-09-12 16:27:35 -0300

    6 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MockArticulosFamiliasService } from './services/mock-articulos-familias.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule,],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
